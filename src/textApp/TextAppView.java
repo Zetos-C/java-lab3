366cc15054f0aa0c36443db94e214c0dac5f116f
Zetos-C <duongdat030@gmail.com>
2024-03-10 16:17:11 +0700

    feat : add scroll bar

--- src/textApp/TextAppView.java
+++ src/textApp/TextAppView.java
@@ -15,6 +15,7 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
 
 public class TextAppView extends JFrame {
 	
@@ -26,6 +27,7 @@ public class TextAppView extends JFrame {
 	JFileChooser fileChoose = new JFileChooser();
 	JTextArea textArea = new JTextArea();
 	private TextAppController controller = new TextAppController(this);
+	private final JScrollPane scrollPane = new JScrollPane();
 
 	/**
 	 * Launch the application.
@@ -61,8 +63,6 @@ public class TextAppView extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		contentPane.add(textArea, BorderLayout.CENTER);
-		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.SOUTH);
 		
@@ -77,5 +77,8 @@ public class TextAppView extends JFrame {
 		panel.add(saveButton);
 		
 		panel.add(saveAsButton);
+		
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		scrollPane.setViewportView(textArea);
 	}
 }
